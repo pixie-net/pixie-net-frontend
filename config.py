6123e36f2736639fd3a2772c0d279e229e4fbc7b
S. V. Paulauskas <stanpaulauskas@gmail.com>
2020-02-16 11:12:38 -0500

    Converting to Jinja2 templating. Finished home page.

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ date: February 15, 2020
 
 class Config(object):
     SITE_NAME = 'Pixie-Net'
+    HOST = '0.0.0.0'
+    PORT = '5001'
+    SECRET_KEY = 'a-super-secret-key-that-nobody-will-discover'
 
     DEBUG = False
     TESTING = False
--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, abort
+"""
+file: run.py
+brief: Main entry for the Pixie-Net front end
+author: S. V. Paulauskas
+date: February 16, 2020
+"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,41 +16,26 @@ else:
     app.config.from_object("config.TestingConfig")
 
 
-PRODUCTS = {
-    'iphone': {
-        'name': 'iPhone 5S',
-        'category': 'Phones',
-        'price': 699,
-    },
-    'galaxy': {
-        'name': 'Samsung Galaxy 5',
-        'category': 'Phones',
-        'price': 649,
-    },
-    'ipad-air': {
-        'name': 'iPad Air',
-        'category': 'Tablets',
-        'price': 649,
-    },
-    'ipad-mini': {
-        'name': 'iPad Mini',
-        'category': 'Tablets',
-        'price': 549
-    }
-}
-
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', products=PRODUCTS)
+    return render_template('home.html')
 
-@app.route('/product/<key>')
-def product(key):
-    product = PRODUCTS.get(key)
-    if not product:
-        abort(404)
-    return render_template('product.html', product=product)
+
+@app.route("/setup")
+def setup():
+    return render_template('setup.html')
+
+
+@app.route("/daq")
+def daq():
+    return render_template('daq.html')
+
+
+@app.route("/analysis")
+def analysis():
+    return render_template('analysis.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=app.config['HOST'], port=app.config['PORT'])
